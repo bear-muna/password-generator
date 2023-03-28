e6ba85d5ad5021b77934675448f7694f69733eb3
bear-muna <bearmuna@gmail.com>
2023-03-28 15:15:48 -0700

    trying user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,40 @@
+// Generate a password that takes in multiple types of parameters
+// With multiple parameters, would I throw parameters into the funtion?
+// ??????? Give a prompt, answer to prompt will then given boolean expression
+// Comabination of boolean expressions will give us structure of characters to use in password
+
+
+// Length
+// ******* Length is user input *******
+  // Gives prompt
+  // Rerun prompt if input is outside of constraints
+// Upper, Lower, Symbols, Numbers
+// Boolean values
+// ???????? Objects for all the values ????????
+// ???????? Objects hold characters and boolean for properites ???????
+
+
+
+// length is a user input
+var length = {
+  inputLength: null,
+  min: 5,
+  max: 20,
+  bool: false,
+  input: function () {
+    while (this.inputLength != Number) {
+       this.inputLength = prompt("How long is the password?");
+    }
+    return;
+  }
+}
+
+
+function generatePassword() {
+  length.input();
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
